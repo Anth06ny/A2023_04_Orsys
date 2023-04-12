9bed430433fac5e6d427e8a64b89fe0a6bb93eb1
Anthony <anth06ny@gmail.com>
2023-04-12 16:51:19 +0200

    Correction TP ManipXML Partie 1

--- app/src/main/java/com/amonteiro/a2023_04_orsys/MainActivity.kt
+++ app/src/main/java/com/amonteiro/a2023_04_orsys/MainActivity.kt
@@ -2,12 +2,28 @@ package com.amonteiro.a2023_04_orsys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.amonteiro.a2023_04_orsys.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    //Créer composants graphiques
+    //by lazy action a retardement -> à la 1ere utilisation
+    val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //afficher
+        setContentView(binding.root)
+
+        //Intercepter les clics des boutons
+        binding.btValider.setOnClickListener {
+            binding.et.setText("Clic sur valider")
+        }
+
+        binding.btCancel.setOnClickListener {
+            binding.et.setText("Clic sur Annuler")
+        }
 
     }
 
